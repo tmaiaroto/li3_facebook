0debc9212adcf2a2ceeaee7f0baba10f8cb6d6a7
Marc Schwering <mschwering@weluse.de>
2011-04-05 11:26:58 +0200

    fixes lithium coding convention errors.

--- extension/FacebookProxy.php
+++ extension/FacebookProxy.php
@@ -3,85 +3,81 @@
 namespace li3_facebook\extension;
 
 use lithium\core\Libraries;
-use lithium\core\Environment;
+//use lithium\core\Environment;
 
 use Exception;
 use lithium\core\ClassNotFoundException;
 
 /**
- * The `FacebookProxy` class handles all Facebook related functionalities. 
- * The class is mainly a lithium wrapper for the existing Facebook API. It is oriented by
- * the proxy-pattern which is using the original FB-API as a singleton.
- * It has to be configured by an api key and the secret.
- * 
- */
+* The `FacebookProxy` class handles all Facebook related functionalities.
+* The class is mainly a lithium wrapper for the existing Facebook API. It is oriented by
+* the proxy-pattern which is using the original FB-API as a singleton.
+* It has to be configured by an api key and the secret.
+*/
 class FacebookProxy extends \lithium\core\StaticObject {
-	
+
 	/**
 	 * Holds the configuration Options
-	 * @var array 
-	 */
+	 * @var array
+	*/
 	protected static $_config = array();
-	
+
 	/**
 	 * These are the class `defaults`
-	 * @var array 
+	 * @var array
 	 */
 	protected static $_defaults = array(
 		'appId' => '',
 		'secret' => '',
 		'cookie' => false,
 		'domain' => false,
-		'fileUpload' => false,
+		'fileUpload' => false
 	);
-	
-	
+
 	/**
 	 * If true, class will automatically fetch data from libraries settings
 	 * Set this to false if you want to do configuration manually or in debug mode
 	 * @var boolean
 	 */
 	public static $_autoConfigure = true;
-	
+
 	/**
 	 * If false, given Config wont be validated
 	 * @var boolean
 	 */
 	public static $_validateConfiguration = true;
-	
-	
+
 	/**
 	 * Holds the Facebook Api Version Strings. associated to the
 	 * tested git hash (just for debug info)
-	 * @var array of Version Strings 
+	 * @var array of Version Strings
 	 */
 	public static $__compatibleApiVersions = array(
-		'2.1.2' => '04168d544f71293fab7622fa81161eef51db808e',
+		'2.1.2' => '04168d544f71293fab7622fa81161eef51db808e'
 	);
-	
+
 	/**
 	 * Holds the FacebookAPI as singleton
-	 * @var Facebbok  
+	 * @var Facebbok
 	 */
 	public static $_facebookApiInstance = null;
-	
-	
+
 	public function __construct($config = array()) {
-		if($config){
+		if ($config){
 			static::config($config);
 		}
 	}
-	
+
 	/**
 	 *
-	 * @return void 
+	 * @return void
 	 */
 	public static function __init() {
-		if(static::$_autoConfigure){
+		if (static::$_autoConfigure){
 			$libraryConfig = Libraries::get('li3_facebook');
 			static::config($libraryConfig + static::$_defaults);
 		}
-		
+
 		/* wont work */
 		/*
 		 static::applyFilter('invokeMethod', function($self, $params, $chain) {
@@ -89,17 +85,15 @@ class FacebookProxy extends \lithium\core\StaticObject {
 			die("awesome");
 			$response = $chain->next($self, $params, $chain);
 			return $response;
-			
 			});
-		 * 
+		 *
 		 */
 	}
-	
-	
+
 	/**
 	 * Sets configurations for the FacebookApi.
 	 * This Method is basically a copy and edit of the config in adaptable.
-	 * 
+	 *
 	 * @see lithium\core\adaptable
 	 *
 	 * @param array $config Configuratin.
@@ -109,8 +103,8 @@ class FacebookProxy extends \lithium\core\StaticObject {
 		//set if `config`is given
 		if ($config && is_array($config)) {
 			//filter only accepts configuration options
-			foreach($config as $key => $value){
-				if(\array_key_exists($key, static::$_defaults)){
+			foreach ($config as $key => $value){
+				if (\array_key_exists($key, static::$_defaults)){
 					static::$_config[$key] = $value;
 				}
 			};
@@ -122,10 +116,10 @@ class FacebookProxy extends \lithium\core\StaticObject {
 			return static::_config($config);
 		}
 		 */
-		
+
 		//set and filter
 		//$result = array();
-		
+
 		//due false and unset Values we disable the filtering:
 		//static::$_config = array_filter(static::$_config);
 		//we dont use named configs (now)
@@ -148,28 +142,27 @@ class FacebookProxy extends \lithium\core\StaticObject {
 		static::$_facebookApiInstance = null;
 		static::$_config = array();
 	}
-	
-	
+
 	/**
 	 * Does proxying the method calls
 	 * @param string $method
-	 * @param mixed $arguments 
+	 * @param mixed $arguments
 	 */
 	public static function __callStatic($method, $arguments) {
 		return static::run($method,$arguments);
 	}
-	
+
 	/**
 	 * Calls should be rerouted to the facebookApiInstance of the proxy
 	 * @todo insert a callable existance check
-	 * 
+	 *
 	 * @see lithium/core/StaticObject
-	 * 
+	 *
 	 * @throws FacebookApiException
-	 * 
-	 * @param string $mehtod
-	 * @param mixed $arguments
-	 * @return mixed
+	 *
+	 * @param string $method
+	 * @param mixed $params
+	 * @return mixed Return value of the called api method
 	 * @filter this Method is filterable
 	 */
 	public static function run($method, $params = array()) {
@@ -177,13 +170,13 @@ class FacebookProxy extends \lithium\core\StaticObject {
 		return static::_filter(__FUNCTION__, $params, function($self, $params) {
 			extract($params);
 
-			if(!$self::$_facebookApiInstance){
+			if (!$self::$_facebookApiInstance){
 				$self::invokeMethod('instanciateFacebookApi');
 			}
 
 			//@todo: insert callable existance check here!
-			if(!\is_callable(array($self::$_facebookApiInstance,$method))){
-				throw new Exception(__CLASS__." Method `$method` is not callable");
+			if (!\is_callable(array($self::$_facebookApiInstance,$method))){
+				throw new Exception(__CLASS__ . " Method `$method` is not callable");
 			}
 
 			switch (count($params)) {
@@ -194,23 +187,29 @@ class FacebookProxy extends \lithium\core\StaticObject {
 				case 2:
 					return $self::$_facebookApiInstance->$method($params[0], $params[1]);
 				case 3:
-					return $self::$_facebookApiInstance->$method($params[0], $params[1], $params[2]);
+					return $self::$_facebookApiInstance->$method(
+						$params[0], $params[1], $params[2]
+					);
 				case 4:
-					return $self::$_facebookApiInstance->$method($params[0], $params[1], $params[2], $params[3]);
+					return $self::$_facebookApiInstance->$method(
+						$params[0], $params[1], $params[2], $params[3]
+					);
 				case 5:
-					return $self::$_facebookApiInstance->$method($params[0], $params[1], $params[2], $params[3], $params[4]);
+					return $self::$_facebookApiInstance->$method(
+						$params[0], $params[1], $params[2], $params[3], $params[4]
+					);
 				default:
 					//i am not sure if this is a good idea
 					return call_user_func_array(array(get_called_class(), $method), $params);
 			}
 		});
 	}
-	
+
 	/**
 	 * Does savely instanciating the Facebook Api.
 	 * @throws Exception for various Errors.
-	 * 
-	 * @param array $config 
+	 *
+	 * @param array $config
 	 * @return Facebook $apiInstance
 	 * @filter This method may be filtered.
 	 */
@@ -219,31 +218,31 @@ class FacebookProxy extends \lithium\core\StaticObject {
 		return static::_filter(__FUNCTION__, $params, function($self, $params) {
 			extract($params);
 
-			if($self::$_validateConfiguration){
+			if ($self::$_validateConfiguration){
 				$self::invokeMethod('checkConfiguration',$config);
 			}
-			if($config){
+			if ($config){
 				$self::config($config);
 			}
 			$self::invokeMethod('_checkApiAvailability');
 			$self::invokeMethod('_requireFacebookApi');
 			$self::invokeMethod('_checkApiCompatibility');
 			$apiInstance = new \Facebook($self::config());
-			if(!$apiInstance){
+			if (!$apiInstance){
 				throw new Exception('Facebook Api cant instanciated!');
 			}
 			$self::$_facebookApiInstance = $apiInstance;
 			return $apiInstance;
 		});
 	}
-	
+
 	/**
 	 * checks the configuration against Problems (and unsupported features)
-	 * 
+	 *
 	 * @todo finish this!
-	 * 
+	 *
 	 * @throws Exceptions if there are problems
-	 * 
+	 *
 	 * @param array $config
 	 * @return boolean
 	 * @filter This method may be filtered.
@@ -253,38 +252,38 @@ class FacebookProxy extends \lithium\core\StaticObject {
 		return static::_filter(__FUNCTION__, $params, function($self, $params) {
 			extract($params);
 
-			if(!$config){
+			if (!$config){
 			$config = $self::invokeMethod('config');
 			}
-			if(empty($config['appId'])){
+			if (empty($config['appId'])){
 				throw new Exception('Configuration: `appId` should be set');
 			}
-			if(empty($config['secret'])){
+			if (empty($config['secret'])){
 				throw new Exception('Configuration: `secret` should be set');
 			}
-			if(!empty($config['cookie'])){
+			if (!empty($config['cookie'])){
 				throw new Exception('Configuration: `cookie` not yet supported');
 			}
-			if(!empty($config['domain'])){
+			if (!empty($config['domain'])){
 				throw new Exception('Configuration: `domain` not yet supported');
 			}
-			if(!empty($config['fileUpload'])){
+			if (!empty($config['fileUpload'])){
 				throw new Exception('Configuration: `fileUpload` not yet supported');
 			}
 			return true;
 		});
 	}
-	
+
 	/**
 	 * Fetches the ApiPath and checks if the Api is there
 	 */
 	protected static function _checkApiAvailability(){
 		$fbLib = static::_getApiPath();
-		if(!\file_exists($fbLib)){
+		if (!\file_exists($fbLib)){
 			throw new ClassNotFoundException('Facebook Lib not there! Do git submoule init first!');
 		}
 	}
-	
+
 	/**
 	 * constructs the Api Path by this file
 	 * @return string full Path to the FacebookApi
@@ -296,34 +295,38 @@ class FacebookProxy extends \lithium\core\StaticObject {
 			extract($params);
 
 			$currentPath = dirname(__FILE__);
-			$fbLib = $currentPath.'/../libraries/facebook-sdk/src/facebook.php';
+			$fbLib = $currentPath . '/../libraries/facebook-sdk/src/facebook.php';
 			return \realpath($fbLib);
 		});
 	}
-	
+
 	/**
-	 * Requires the Facebok Api
+	 * Requires the Facebok Api.
+	 *
 	 * @throws (rethrows) Exception if curl or json_decode not reachable!
-	 * 
+	 * @return void
 	 */
 	protected static function _requireFacebookApi(){
 		require_once static::_getApiPath();
 	}
-	
+
 	/**
 	 * Checks the ApiVersion against this Proxy capabilities
+	 *
 	 * @throws Exception if the Library is not compatible
+	 * @return void
 	 */
 	protected static function _checkApiCompatibility(){
 		$versions = static::$__compatibleApiVersions;
-		if(!\array_key_exists(\Facebook::VERSION, $versions)){
+		if (!\array_key_exists(\Facebook::VERSION, $versions)){
 			throw new Exception('Facebook Library is not compatible to our library');
 		}
 	}
-	
+
 	/**
 	 * Returns the instaciated Facebook Class for own usage.
-	 * @return Facebook $facebookInstance 
+	 *
+	 * @return Facebook $facebookInstance
 	 */
 	public static function getApiInstance(){
 		return static::$_facebookApiInstance;
--- tests/cases/extension/FacebookProxyTest.php
+++ tests/cases/extension/FacebookProxyTest.php
@@ -8,154 +8,153 @@ use lithium\core\Libraries;
 use Exception;
 
 class FacebookProxyTest extends \lithium\test\Unit {
-	
+
 	protected $_mockDefaults = array(
 		'appId' => '',
 		'secret' => '',
 		'cookie' => false,
 		'domain' => false,
-		'fileUpload' => false,
+		'fileUpload' => false
 	);
 
 	public function setUp() {}
 
 	public function tearDown() {}
-	
+
 	public function testReset(){
 		FacebookProxy::reset();
 		$this->assertEqual(FacebookProxy::config(), array(), 'configuration should be empty.');
 	}
-	
+
 	//test configuration
 	public function testConfig(){
 		$oldConfig = Libraries::get('li3_facebook');
-		
+
 		Libraries::remove('li3_facebook');
 		Libraries::add('li3_facebook');
-		
-		FacebookProxy::$_autoConfigure = false;		
+
+		FacebookProxy::$_autoConfigure = false;
 		FacebookProxy::__init();
-		
+
 		$this->assertEqual(FacebookProxy::config(),array(), 'config should be empty.');
-		
+
 		$this->assertEqual(FacebookProxy::config(array()),array(), 'config should be empty.');
-		
+
 		//check ignoring
 		FacebookProxy::reset();
 		$result = FacebookProxy::config(array('foo'));
 		$this->assertTrue($result,array(),'config should return true');
 		$this->assertIdentical(FacebookProxy::config(),array(),'config should be empty');
-		
+
 		//check ingoring vs. existing but unset associations
 		FacebookProxy::reset();
 		$result = FacebookProxy::config(array('appId'));
 		$this->assertTrue($result,array(),'config should return true');
 		$this->assertIdentical(FacebookProxy::config(),array(),'config should be empty');
-		
+
 		//check valid Settings
 		FacebookProxy::reset();
 		$sampleConfig = array('appId'=>'hello');
 		$result = FacebookProxy::config($sampleConfig);
 		$this->assertTrue($result,'config should return true');
 		$this->assertIdentical(FacebookProxy::config(),$sampleConfig,'config should not be empty');
-		
+
 		//check vs. complete Settings
 		FacebookProxy::reset();
 		$result = FacebookProxy::config($this->_mockDefaults);
 		$this->assertTrue($result,'config should return true');
-		$this->assertIdentical(FacebookProxy::config(),$this->_mockDefaults,'config should not be empty');
-
+		$this->assertIdentical(
+			FacebookProxy::config(),$this->_mockDefaults,'config should not be empty'
+		);
 
 		//FaceBookProxy::foo();
 		//die(print_r(array($result,FacebookProxy::config()),true));
-		
 	}
-	
+
 	public function testCheckConfiguration(){
 		//invalid Settings
 		$invalidSettings = array(
 			array(
 				'config' => array(),
-				'exception' => 'Configuration: `appId` should be set',
+				'exception' => 'Configuration: `appId` should be set'
 			),
 			array(
 				'config' => array('appId' => 'foo'),
-				'exception' => 'Configuration: `secret` should be set',
+				'exception' => 'Configuration: `secret` should be set'
 			),
 			array(
 				'config' => array(
 					'appId' => 'foo',
 					'secret' => 'bar',
-					'cookie' => true,
+					'cookie' => true
 				),
-				'exception' => 'Configuration: `cookie` not yet supported',
+				'exception' => 'Configuration: `cookie` not yet supported'
 			),
 			array(
 				'config' => array(
 					'appId' => 'foo',
 					'secret' => 'bar',
-					'domain' => true,
+					'domain' => true
 				),
-				'exception' => 'Configuration: `domain` not yet supported',
+				'exception' => 'Configuration: `domain` not yet supported'
 			),
 			array(
 				'config' => array(
 					'appId' => 'foo',
 					'secret' => 'bar',
-					'fileUpload' => true,
+					'fileUpload' => true
 				),
-				'exception' => 'Configuration: `fileUpload` not yet supported',
-			),
-			
+				'exception' => 'Configuration: `fileUpload` not yet supported'
+			)
 		);
 		$validSettings = array(
 			array(
 				'config' => array(
 					'appId' => 'foo',
+					'secret' => 'bar'
+				)
+			),
+			array(
+				'config' => array(
+					'appId' => 'foo',
 					'secret' => 'bar',
-				),
+					'cookie' => false
+				)
+			),
+			array(
+				'config' => array(
+					'appId' => 'foo',
+					'secret' => 'bar',
+					'domain' => false
+				)
+			),
+			array(
+				'config' => array(
+					'appId' => 'foo',
+					'secret' => 'bar',
+					'fileUpload' => false
+				)
+			),
+			array(
+				'config' => array(
+					'appId' => 'foo',
+					'secret' => 'bar',
+					'domain' => false,
+					'fileUpload' => false
+				)
 			),
 			array(
 				'config' => array(
 					'appId' => 'foo',
 					'secret' => 'bar',
 					'cookie' => false,
-				),
-			),
-			array(
-				'config' => array(
-					'appId' => 'foo',
-					'secret' => 'bar',
-					'domain' => false,
-				),
-			),
-			array(
-				'config' => array(
-					'appId' => 'foo',
-					'secret' => 'bar',
-					'fileUpload' => false,
-				),
-			),
-			array(
-				'config' => array(
-					'appId' => 'foo',
-					'secret' => 'bar',
-					'domain' => false,
-					'fileUpload' => false,
-				),
-			),
-			array(
-				'config' => array(
-					'appId' => 'foo',
-					'secret' => 'bar',
-					'cookie' => false,
-					'domain' => false,
-				),
-			),
+					'domain' => false
+				)
+			)
 		);
-		
+
 		$proxy = new FacebookProxy();
-		foreach($invalidSettings as $test){
+		foreach ($invalidSettings as $test){
 			try {
 				//$this->expectException($test['exception']);
 				$proxy::checkConfiguration($test['config']);
@@ -164,31 +163,30 @@ class FacebookProxyTest extends \lithium\test\Unit {
 				$this->assertIdentical($test['exception'], $e->getMessage(),$test['exception']);
 			}
 		}
-		foreach($validSettings as $test){
+		foreach ($validSettings as $test){
 			$this->assertTrue($proxy::checkConfiguration($test['config']));
 		}
 	}
-	
-	
+
 	public function testProxying(){
 		//$this->skipIf(True);
 		//$this->assertFalse(FacebookProxy::__init());
 		$fb = new FacebookProxy();
 		$fb::$_validateConfiguration = false;
-		
-		
+
 		$this->assertIdentical('', $fb::run('getAppId'));
-		
+
 		$fb::reset();
 		$expected = array(
 			'appId' => 'foo',
-			'secret' => 'bar',
+			'secret' => 'bar'
 			);
 		$fb::config($expected);
 		$this->assertIdentical($expected['appId'], $fb::run('getAppId'));
 		$this->assertIdentical($expected['secret'], $fb::run('getApiSecret'));
-		
-		$expectedMessage = 'li3_facebook\extension\FacebookProxy Method `unknownApiCall` is not callable';
+
+		$expectedMessage = 'li3_facebook\extension\FacebookProxy Method ' .
+			'`unknownApiCall` is not callable';
 		$this->expectException($expectedMessage);
 		$this->assert($fb::run('unknownApiCall'));
 	}
