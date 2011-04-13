31e5ef152aaee0f18226a54776d6a3ff9b195a20
dgAlien <pti.ms@gmx.net>
2011-04-14 00:24:30 +0200

    First test shot.

--- tests/cases/extensions/adapter/security/auth/FacebookTest.php
+++ tests/cases/extensions/adapter/security/auth/FacebookTest.php
@@ -8,11 +8,40 @@
 
 namespace li3_facebook\tests\cases\extensions\adapter\security\auth;
 
-use li3_facebook\tests\mocks\extensions\adapter\security\auth\MockFacebook;
+use li3_facebook\tests\mocks\extensions\adapter\security\auth\MockFacebook as Facebook;
+use li3_facebook\extensions\FacebookProxy;
 use lithium\action\Request;
+use lithium\core\Libraries;
+use lithium\core\ConfigException;
 
 class FacebookTest extends \lithium\test\Unit {
 	
+	/**
+	 * @var Request $request Object
+	 */
+	public $request;
 	
+	public function setUp() {
+		$this->request = new Request();
+	}
+
+	public function tearDown() {}
+
+	//test an empty facebook config => should throw an Exception
+	public function testEmptyConfig(){
+		$oldConfig = Libraries::get('li3_facebook');
+		$subject = new Facebook();
+		//disable validation inside the proxy
+		FacebookProxy::$_validateConfiguration = false;
+		
+		Libraries::remove('li3_facebook');
+		Libraries::add('li3_facebook');
+		
+		$this->expectException('Configuration: `appId` should be set');
+		$this->assertTrue($subject->check($this->request));
+		
+		Libraries::remove('li3_facebook');
+		Libraries::add('li3_facebook',$oldConfig);
+	}
 }
 ?>
