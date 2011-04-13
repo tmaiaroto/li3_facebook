2ef4f089d949a349f543754195ddeba3b7aa901f
dgAlien <pti.ms@gmx.net>
2011-04-13 22:44:50 +0200

    created mock and test skeletons for the Facebook adapter

---
+++ tests/cases/extensions/adapter/security/auth/FacebookTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_facebook\tests\cases\extensions\adapter\security\auth;
+
+use li3_facebook\tests\mocks\extensions\adapter\security\auth\MockFacebook;
+use lithium\action\Request;
+
+class FacebookTest extends \lithium\test\Unit {
+	
+	
+}
+?>
---
+++ tests/mocks/extensions/adapter/security/auth/MockFacebook.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * li3_facebook Adapter Mockup
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_facebook\tests\mocks\extensions\adapter\security\auth;
+
+class MockFacebook extends \li3_facebook\extensions\adapter\security\auth\Facebook {
+	
+}
+
+?>
